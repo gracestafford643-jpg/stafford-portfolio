2919fbb9d79a7a9e15499d38b66429d9f30b4df8
Grace Stafford <gracestafford643@gmail.com>
2025-10-22 10:16:45 -0700

    Update Navbar.jsx and App.js

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import './Navbar.css';
 export default function Navbar() {
   return (
     <nav className="navbar">
-      <h2 className="logo">Grace Stafford - Software Devloper</h2>
+      <h2 className="logo">Grace Stafford - Software Developer</h2>
       <ul>
         <li><a href="#about">About</a></li>
         <li><a href="#experience">Resume</a></li>
